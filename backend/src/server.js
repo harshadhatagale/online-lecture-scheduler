70a31647f56c068daa91aab6bf4fc65015877796
harshadhatagale <harshadhatagale6@gmail.com>
2026-05-29 12:48:10 +0530

    cors issue fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,30 +1,70 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
-require("dotenv").config()
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const app = express()
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.json())
+const app = express();
 
-app.use('/api/auth', require("./routes/authRoutes"))
-app.use("/api/courses", require("./routes/courseRoutes"))
-app.use("/api/batches", require("./routes/batchRoutes"))
-app.use("/api/lectures", require("./routes/lectureRoutes"))
+// CORS
 app.use(
-   "/api/users",
-   require("./routes/userRoutes")
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://online-lecture-scheduler.vercel.app",
+    ],
+    credentials: true,
+  })
 );
-mongoose.connect(process.env.MONGO_URI).then(() => {
 
-   console.log("MongoDB Connected")
-  const PORT = process.env.PORT || 5000;
-   app.listen(PORT, () => {
-       console.log(`Server running on port ${PORT}`);
-   })
+app.use(express.json());
 
-})
-   .catch((err) => {
-      console.log(err)
-   })
+// Routes
+app.use(
+  "/api/auth",
+  require("./routes/authRoutes")
+);
+
+app.use(
+  "/api/courses",
+  require("./routes/courseRoutes")
+);
+
+app.use(
+  "/api/batches",
+  require("./routes/batchRoutes")
+);
+
+app.use(
+  "/api/lectures",
+  require("./routes/lectureRoutes")
+);
+
+app.use(
+  "/api/users",
+  require("./routes/userRoutes")
+);
+
+// Health Check Route
+app.get("/", (req, res) => {
+  res.send("API Running...");
+});
+
+// DB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+
+    const PORT =
+      process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(
+        `Server running on port ${PORT}`
+      );
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
